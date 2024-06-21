f51ff03edd866a451bc1d981a693e80fe27a9a77
Alexander Grenkov <a.grenkov@gmail.com>
2024-06-21 19:23:12 +0500

    fix: account app and add tests

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+
+CustomUser = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
@@ -11,11 +14,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = [
         "email",
         "username",
-        "age",
-        "is_staff",
+        "is_superuser",
     ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = CustomUser
+    class Meta:
+        model = get_user_model()
         fields = (
-            'username',
-            'email',
-            'age',
+            "email",
+            "username",
         )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = (
-            'username',
-            'email',
-            'age',
+            "email",
+            "username",
         )
