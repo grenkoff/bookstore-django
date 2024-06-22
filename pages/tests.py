b370e465b0f7b10954267014f21b8e24e16b91c6
Alexander Grenkov <a.grenkov@gmail.com>
2024-06-22 05:17:00 +0500

    add: home page and tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,14 +1,26 @@
 from django.test import SimpleTestCase
 from django.urls import reverse
 
+from .views import HomePageView
+
 
 class HomePageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location_homepageview(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
 
-    def test_homepage_view(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "home.html")
-        self.assertContains(response, "Home")
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "home page")
+    
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
